58f3e151b0c952df7b7b1d3e71bc5d4d0ebb03aa
Araceli <araasilva96@gmail.com>
2021-09-21 14:38:20 -0300

    a completar

---
+++ Tp02/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Tp02.Models;
+
+namespace Tp02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly Logger _logger;
+
+        public HomeController(ILogger<HomeController> logger, Logger log)
+        {
+            _logger = log;
+            _logger.Debug("NLog injected into HomeController");
+        }
+
+        public IActionResult Index()
+        {
+            _logger.Debug("Hello, this is the index!");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult practico2(string nombre, string apellido, string fechaNacimiento, string direccion, string fechaIngreso)
+        {
+            try
+            {
+                if (nombre != null && apellido != null && fechaNacimiento != null && direccion != null && fechaIngreso != null)
+                {
+                    DateTime fNacimiento = DateTime.Parse(fechaNacimiento);
+                    DateTime fIngreso = DateTime.Parse(fechaIngreso);
+
+                    Empleado empleado = new Empleado(nombre, apellido, direccion, fNacimiento, fIngreso);
+                    _logger.Info("Datos guardados: " + nombre + " " + apellido + ", " + direccion + ", " + fNacimiento + ", " + fIngreso);
+                    ViewBag.Empleado = empleado;
+                    return View(empleado);
+                }
+                else
+                {
+                    _logger.Error("Se ingresaron campos vacios");
+                    ViewBag.mensaje = "Error al ingresar los datos";
+                }
+            }
+
+            catch (Exception ex)
+            {
+                _logger.Error(ex.Message);
+                ViewBag.mensaje = "Error al procesarse los datos";
+            }
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Tp02/Models/Empleado.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tp02.Models
+{
+    public class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private string direccion;
+        private DateTime fechaNacimiento;
+        private DateTime fechaIngreso;
+        private int antiguedad;
+        private const float sueldoBasico = 28080;
+        private int edad;
+        private double salario;
+        
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+        public int Antiguedad { get => antiguedad; set => antiguedad = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public double Salario { get => salario; set => salario = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public Empleado(string nombre, string apellido, string direccion, DateTime fechaNacimiento, DateTime fechaIngreso)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.direccion = direccion;
+            this.fechaNacimiento = fechaNacimiento;
+            this.fechaIngreso = fechaIngreso;
+            this.antiguedad = CalcularEdad(fechaIngreso);
+            this.edad = CalcularEdad(fechaNacimiento);
+            this.salario = CalcularSalario(antiguedad);
+        }
+
+        public int CalcularEdad(DateTime fecha)
+        {
+            int edad = (int)(((DateTime.Now - fecha).Days)/365.25);
+            return edad;
+        }
+
+        public double CalcularSalario( int antiguedad)
+        {
+            double Salario;
+            if (antiguedad > 20)
+            {
+                Salario = sueldoBasico + (sueldoBasico * 0.25)- (sueldoBasico* 0.15);
+            }
+            else
+            {
+                Salario = sueldoBasico + (sueldoBasico * (antiguedad/100) - (sueldoBasico * 0.15)) ;         
+            }
+            return Salario;
+        }
+    }
+}
